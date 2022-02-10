6c0fe2372d7f30ef655c448f3444bfcfeb93115e
ayebravo <ayelenbravo@gmail.com>
2022-02-10 16:28:44 -0600

    Created React component called Addition
    The functional component receives two props that are numbers and adds them. The app displays both numbers besides the sum of both. Added also some styling using CSS-in-JS.
    
    The project 02 react app is part of my college class "Adv. Cloud for Developers" where one of the learning components is using React

---
+++ src/Addition.js
@@ -0,0 +1,11 @@
+const Addition = ({ numberOne, numberTwo }) => {
+	return (
+		<>
+			<h2>
+				{numberOne} + {numberTwo} = {numberOne + numberTwo}
+			</h2>
+		</>
+	);
+};
+
+export default Addition;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import Addition from "./Addition";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = {
+	container: {
+		backgroundColor: "#113f67",
+		padding: "1em",
+		color: "#e7eaf6",
+	},
+	heading: {
+		color: "#ECB365",
+	},
+};
+
+const App = () => {
+	return (
+		<div style={styles.container}>
+			<h1 style={styles.heading}>Addition App</h1>
+			<Addition numberOne={10} numberTwo={35} />
+			<Addition numberOne={2} numberTwo={48} />
+			<Addition numberOne={100} numberTwo={57} />
+			<Addition numberOne={992} numberTwo={8} />
+			<Addition numberOne={5} numberTwo={7} />
+		</div>
+	);
+};
 
 export default App;
